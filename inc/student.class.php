9adc0c1116a384ab170d5df5611e2bd4763c24d1
guowushi <guowushi@hotmail.com>
2013-06-12 16:02:37 +0800

    images css

---
+++ inc/student.class.php
@@ -0,0 +1,87 @@
+<?php
+   class student{
+   
+public $classes;
+public $StudentName;
+public $sex ;
+public $IDType ;
+public $IDCard;
+public $nationality;
+public $birth;
+public $RuXueNianYue;
+public $EntranceWay ;
+public $XueJiHao;
+public $schoolCode;
+public $IDExpiredDate;
+public $HuJiDi;
+public $jdcategory;
+public $JiuDuFangShi;
+public $XueShengLaiYuan;
+public $schoolGraduation;
+public $entranceScores;
+public $usedName;
+public $nativePlace;
+public $BirthAddress;
+public $LiveAddress;
+public $PostalAddress;
+public $postalCode;
+public $telephone;
+public $email;
+public $ZhuYeDiZhi;
+public $GangTaiQiaoWai;
+public $bloodType;
+public $health;
+public $politicsStatus; 
+public $SuiBanJiuDu;
+public $disability;
+public $speciality;
+public $MaiXueWei;
+public $LeftoverChildren;
+public $onlyChild;
+public $MigrantChildren; 
+public $DiBao;
+public $SingleFamily;
+public $BuZhu;
+public $JiaoShiZiNv;
+public $MilitaryChildren;
+public $XueQianJiaoYu;
+public $orphan;
+public $LieShi;
+public $applyForFunding;
+public $HasSubsidy;
+public $distance;
+public $transportation;
+public $SchoolBus;
+public $F_name;
+public $F_nationality;
+public $F_tel;
+public $F_workPlace;
+public $F_hujidi;
+public $F_address;
+public $F_IDtype;
+public $F_IDcard;
+public $F_job ;
+public $M_name;
+public $M_nationality;
+public $M_tel; 
+public $M_workPlace;
+public $M_hujidi;
+public $M_address;
+public $M_IDtype;
+public $M_IDcard;
+public $M_job ; 
+public $Q_name;
+public $Q_nationality;
+public $Q_tel;
+public $Q_workPlace;
+public $Q_hujidi; 
+public $Q_address;
+public $Q_IDtype;
+public $Q_IDcard;
+public $Q_job  ; 
+public $Q_Introductions;
+   
+   
+   }
+
+?>
