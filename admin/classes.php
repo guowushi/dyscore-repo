02a0bf3b161b66b727c880decd01073eaaf59295
zhongzilu <zhongzilu520@163.com>
2013-06-18 22:00:07 +0800

    查看所有班级信息

--- admin/classes.php
+++ admin/classes.php
@@ -1,8 +1,10 @@
-<?php
-	require_once dirname(__FILE__) . '/../conn.php';
-	require_once dirname(__FILE__) . '/../libs/medoo.min.php'; 
-	$database = new medoo("dyscore");
-?>
+<?php	
+	session_start();												// 启用此页面的会话功能
+	require_once '../global.inc';										// 包含系统配置文件
+	require_once ROOT.'/inc/functions.php';								// 包含通用函数文件	
+	require_once ROOT.'/libs/medoo.min.php'; 						// 引用用medoo框架类，可以简化数据库的操作（数据用户名和密码在此文件中修改） 
+	$database = new medoo("dyscore");								// 连接到dyscore数据库	
+ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -17,20 +19,28 @@ h1{text-align:center;}
 </head>
 
 <body>
-  <?php
- require_once dirname(__FILE__) .  '/../conn.php'; 
- 
- 
- ?>
    <form method="post" action="">
       <h1>各校班级信息表</h1>
       <div class="head">
       <label>请选择学生所属学校：</label>
-      <select>
-      <option>德阳七中</option>
-      <option>德阳七中</option>
-      <option>德阳七中</option>
-      </select>
+      <select name="SchoolName">
+		 <?php 
+	
+	/*
+		定义查询语句,执行该$sql语句，并获去所有记录放到一个二维数组$rows中
+	*/
+	$sql="select * from schools ";  	
+	$rows=$database->query($sql)->fetchAll();
+	$row_number=1;	
+	// 遍历数组，每行就表示一条记录。访问记录的字段可以使用 $row['字段名']或$row[1]的格式
+	foreach($rows  as $row)
+		{
+  ?>
+           <option value="<?php echo $row["ID"];  ?>"><?php echo $row["ID"].$row["SchoolName"];  ?></option>
+         <?php
+                }
+			?>
+         </select>
       <label>请选择学生师所属年级：</label>
       <select>
       <option>初一</option>
@@ -45,28 +55,36 @@ h1{text-align:center;}
     <th scope="col" name="Classname">班级名称</th>
 	<th scope="col" name="ClassDel">删除班级信息</th>
   </tr>
-   <?php 
-  	$sql="select * from classes ";
-		$rs = $db->query($sql);
-		$i=1;
-		while($row = $rs->fetch())
+  <?php 
+	
+	/*
+		定义查询语句,执行该$sql语句，并获去所有记录放到一个二维数组$rows中
+	*/
+	$sql="select * from classes ";  	
+	$rows=$database->query($sql)->fetchAll();
+	$row_number=1;	
+	// 遍历数组，每行就表示一条记录。访问记录的字段可以使用 $row['字段名']或$row[1]的格式
+	foreach($rows  as $row)
 		{
-			
-			
-		 
-		
-  
   ?>
   <tr>
-    <td><?php echo $i;?></td>
-    <td><?php echo $row['SchoolID'];?></td>
+    <td><?php echo $row_number;?></td>
+	<?php
+	   //  当前记录的学校编号 $row['SchoolID']
+	   $sql="select * from Schools Where  SchoolCode=".$row['SchoolID'] ; 
+        //var_dump($sql);		
+		$row1=$database->query($sql)->fetchAll();
+		//var_dump($row1);
+		//$row1['SchoolName'];
+	
+	?>
+    <td><?php echo $row1[0]['SchoolName'];?></td>
     <td><?php echo $row['Classname'];?></td>
-	<td><a href="#">编辑</a> |  <a href="ClassDel.php?id=<?php  echo $row["ID"]; ?>">删除</a></td>
+	<td><a href="classesForm.php?id=<?php  echo $row["ID"]; ?>">编辑</a> |  <a href="classDel.php?id=<?php  echo $row["ID"]; ?>">删除</a></td>
   </tr>
   <?php
-    $i=$i+1;
-  }
-  
+    $row_number=$row_number+1;
+  } 
   ?>
 </table>
 
