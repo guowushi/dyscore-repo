d18df43e710ba33777ca91357e26161b8e89fdd0
zhongzilu <zhongzilu520@163.com>
2013-06-18 22:03:51 +0800

    删除学生信息

--- admin/StudentDel.php
+++ admin/StudentDel.php
@@ -1,8 +1,15 @@
+<?php
+	session_start();												// 启用此页面的会话功能
+	require_once '../global.inc';										// 包含系统配置文件
+	require_once ROOT.'/inc/functions.php';								// 包含通用函数文件
+	require_once ROOT.'/libs/medoo.min.php'; 						// 引用用medoo框架类，可以简化数据库的操作（数据用户名和密码在此文件中修改） 
+	$database = new medoo("dyscore");								// 连接到dyscore数据库
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UNTITLE</title>
+<title>删除信息</title>
 <style type="text/css">
 table{background-color:#000;}
 tr{background-color:#FFF; text-align:center;}
@@ -12,19 +19,15 @@ h1{text-align:center;}
 
 <body>
 <?php
- 
- require_once dirname(__FILE__) .  '/../conn.php'; 
-
- //获取要删学生的ID
- 
+	//获取要记录的ID，ID统一使用自动编号的主键.注意大小写
     $ID=$_GET["id"];
-    
+	// 定义并执行删除的SQL语句
  	$sql="DELETE  FROM students WHERE ID =".$ID;
-    
-    //处理一条SQL语句，并返回所影响的条目数
-    $cnt=$db->exec($sql);
+	$database->query($sql);
+	// 也可以使用$database->delete("account", ["ID" =>$ID]);
+	// 执行后，跳转到school.php页面
     alert("删除成功！","Student.php");
-	 
+
 ?>
 </body>
 </html>
