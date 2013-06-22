24fb75e8ad938b74b84e12238a8a39b7a097e414
guowushi <guowushi@qq.com>
2013-06-22 16:17:25 +0800

    OK

--- admin/lessonForm.php
+++ admin/lessonForm.php
@@ -6,9 +6,7 @@
 	require_once ROOT.'/libs/medoo.min.php'; 						// 引用用medoo框架类，可以简化数据库的操作（数据用户名和密码在此文件中修改） 
 	$database = new medoo("dyscore");								// 连接到dyscore数据库
 	
- ?>
-<?php
-
+ 
 	//获取传递过来的记录唯一编号
 	$id= !empty($_GET['id']) ? $_GET['id']:'';
 	if($id){
@@ -20,22 +18,37 @@
 		返回结果是一个二维数据
 		*/
 		$tablename="lessons";			
-		$fields=["SchoolCode","ClassID","TeacherID","Lesson","ItemID","Term","comment"];
+		$fields=$SCHEMAS['lessons'];
 		$conditions=["id" => $id];
 		$data = $database->get($tablename, $fields, $conditions);
-		
-		
 
 	}else{
 		
 		$data['SchoolCode']=!empty($_SESSION['school']['SchoolCode'])?$_SESSION['school']['SchoolCode']:"";
-		 
+
+		$data['ClassID']='';
+		$data['TeacherID']='';
+		$data['Lesson']='';
 		$data['ItemID']='';
 		$data['Term']=$TERMS[0];
 		$data['comment']='';
 		//var_dump($data);
 		//var_dump($_SESSION['school']);
 	
+	}
+	/*
+	 根据$arr数组的值产生option选项
+	 $arr=[
+	     0=>['value'=>'返回值','displayValue'=>'显示值','selected'=>'']
+	 ]
+	
+	*/
+	function CreateOptions($arr){
+		$opt="";
+		foreach($arr as $opt){
+			//echo	"<option value=".opt['value'].">".opt['displayValue']."</option>" 
+		}
+			 
 	}
 	
  ?>
@@ -44,6 +57,20 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title></title>
+<script type="text/javascript" src="/js/jquery-1.10.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/libs/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/css/reset.css">
+<script type="text/javascript">
+  $(function(){
+		 
+  
+  })
+  
+  function validate(){
+		$("#Term").removeAttr("disabled");
+  
+  }
+</script>
 </head> 
 <body>
 
@@ -63,17 +90,24 @@
 			$tablename='classes';			
 			$fields=$SCHEMAS['classes'];
 			if(!empty($data['SchoolCode'])){
-				
 				$conditions=["SchoolCode" => $data['SchoolCode']];
-				 
 			}else{
 				$conditions=[];
 			}
 			//获取当前学校所有班级
 			$rows = $database->select($tablename,$fields , $conditions);
 			foreach($rows as $row){
+				$opt['value']=$row['ClassCode'];
+				$opt['displayValue']=$row['ClassName'];
+
+				$arr[]=$opt;
+			}
+			// 显示班级选项
+			foreach($rows as $row){
+			
 			?>
-				<option value="<?php echo $row['ClassCode'];?>"><?php echo $row['SchoolCode'].'-'.$row['ClassName'];?></option>
+				<option value="<?php echo $row['ClassCode'];?>"  <?php echo  $row['ClassCode']==$data['ClassID']?"selected":"" ?> >
+				<?php echo $row['SchoolCode'].'-'.$row['ClassName'];?></option>
 			<?php
 			}
 			?>
@@ -97,7 +131,7 @@
 			$rows = $database->select($tablename,$fields , $conditions);
 			foreach($rows as $row){
 			?>
-				<option value="<?php echo $row['ID'];?>"><?php echo $row['TeacherName'];?></option>
+				<option value="<?php echo $row['ID'];?>"   <?php echo  $row['ID']==$data['TeacherID']?"selected":"" ?>  ><?php echo $row['TeacherName'];?></option>
 			<?php
 			}
 			?>
@@ -113,7 +147,7 @@
 		 
 			foreach($ITEMS as $item){
 			?>
-				<option value="<?php echo $item;?>"><?php echo $item;?></option>
+				<option value="<?php echo $item;?>"  <?php echo  $item==$data['Lesson']?"selected":"" ?> ><?php echo $item;?></option>
 			<?php
 			}
 			?>
@@ -123,12 +157,12 @@
 	  </tr>
 	  <tr>
 		<td><label>课程编号</label></td>
-		<td><input type="text" name="ItemID"  value="<?php  echo $data['ItemID']; ?>" /></td>
+		<td><input type="text" name="ItemID"  value="<?php  echo $data['ItemID']; ?>" readonly /></td>
 	  </tr>
 	  <tr>
 		<td><label>学期</label></td>
 		<td>
-			<select name="Term" readonly>
+			<select name="Term" id="Term" disabled>
 		<?php
 		 
 			foreach($TERMS as $item){
@@ -147,7 +181,8 @@
 	</table>
 	<input name="id" type="hidden" value="<?php echo $id;?>"/>
 	<input name="ok"  value="提交信息" class="submit"  title="保存学校信息" type="submit" onclick="return validate()" >
-    <input name="dfbgdg" title="取消"  type="button" value="退出"/>
+    <input name="cancel" title="取消"  type="button" value="退出"/>
+	<a href="lesson.php">查看</a>
 </form>
 	
 </body>
